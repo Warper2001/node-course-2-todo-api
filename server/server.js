be2514eba101363ddec4ae910cefac90a0b50c41
Alex Bernstein <warper2001@gmail.com>
2017-07-31 13:04:45 -0500

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -107,7 +107,6 @@ app.post('/users', (req, res) => {
 
 app.post('/users/login', (req, res) => {
   var body = _.pick(req.body, ['email', 'password'])
-
   User.findByCredentials(body.email, body.password).then((user) => {
     return user.generateAuthToken().then((token) => {
       res.header('x-auth', token).send(user)
@@ -119,6 +118,14 @@ app.post('/users/login', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user)
+})
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send()
+  }, () => {
+    res.status(400).send()
+  })
 })
 
 app.listen(port, () => {
